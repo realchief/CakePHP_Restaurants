31174396d838c03cec65f992c1f1412f429f54b8
realchief <stryokka@gmail.com>
2018-09-03 03:07:55 +0000

    implemented adding addons

--- src/Controller/Restaurantadmin/AddonsController.php
+++ src/Controller/Restaurantadmin/AddonsController.php
@@ -68,5 +68,197 @@ class AddonsController extends AppController
         ])->hydrate(false)->toArray();
         $this->set(compact('addonsList'));
     }#index function end...
+
+#-------------------------------------------------------------------------------------------
+    /*Addons Add Section*/
+  public function add(){
+
+      if($this->request->is('post')) {
+          $postData = $this->request->getData();
+          
+          if (!empty($postData['mainaddons_name'])) {
+
+              $mainAddonCount = $this->Mainaddons->find('all', [
+                  'conditions' => [
+                      'mainaddons_name' => trim($postData['mainaddons_name']),
+                      'category_id' => trim($postData['category_id']),
+                  ]
+              ])->first();
+
+              if (empty($mainAddonCount)) {
+
+                  $mainEntity = $this->Mainaddons->newEntity($postData);
+                  $mainEntity->status = '1';
+                  $mainAddon  = $this->Mainaddons->save($mainEntity);                 
+
+                    if (!empty($mainAddon)) {
+                      if (!empty($this->request->getData('Main')['subaddons'])) {
+                        foreach ($this->request->getData('Main')['subaddons'] as $skey => $sval) {
+                            if (!empty($sval['subaddons_name']) && !empty($sval['subaddons_price'])) {
+                                $subEntity = $this->Subaddons->newEntity();
+                                $sub['mainaddons_id']   = $mainAddon->id;
+                                $sub['category_id']     = $postData['category_id'];
+                                $sub['subaddons_name']  = $sval['subaddons_name'];
+                                $sub['subaddons_price'] = $sval['subaddons_price'];
+                                $subPatch = $this->Subaddons->patchEntity($subEntity, $sub);
+                                $this->Subaddons->save($subPatch);
+                                unset($subPatch);
+                            }
+                        }
+                    }
+                    $this->Flash->success(__("Mainaddon details inserted successfully"));
+                    return $this->redirect(ADMIN_BASE_URL.'addons/index'); 
+                  }
+              } else {
+                   $this->Flash->error(__("Mainaddon name already exist"));
+              }
+          } else {
+              $this->Flash->error(__("Please enter mainaddon name"));
+          }
+      }
+
+     //Categories Details
+      $categorylist = $this->Categories->find('list', [
+          'keyField' => 'id',
+          'valueField' => 'category_name',
+          'conditions' => [
+              'status' => 1,
+              'delete_status' => 'N'
+          ],
+      ])->hydrate(false)->toArray();   
+      $this->set(compact('categorylist'));    
+   } 
+  #-------------------------------------------------------------------------------------------
+
+    /*Addons Edit Section*/
+   public function edit($id = null){
+
+       //Categories Details
+       $categorylist = $this->Categories->find('list', [
+           'keyField' => 'id',
+           'valueField' => 'category_name',
+           'conditions' => [
+               'status' => 1,
+               'delete_status' => 'N'
+           ],
+       ])->hydrate(false)->toArray();
+
+       //Edit Details
+       $allAddons = $this->Mainaddons->find('all', [
+          'conditions' => [
+              'Mainaddons.id' => $id
+          ],
+          'contain' => [
+              'Subaddons'
+          ]
+        ])->first();
+
+       //------------------------------------------
+       if($this->request->is(['post','put'])) {
+
+           $postData = $this->request->getData();
+
+           if (!empty($postData['mainaddons_name'])) {
+
+               $mainAddonCount = $this->Mainaddons->find('all', [
+                   'conditions' => [
+                       'id !=' => $id,
+                       'mainaddons_name' => trim($postData['mainaddons_name']),
+                       'category_id' => trim($postData['category_id']),
+                   ]
+               ])->first();
+
+               if (empty($mainAddonCount)) {
+
+                   $mainEntity = $this->Mainaddons->newEntity($postData);
+                   $mainEntity->id = $postData['editid'];
+                   $mainAddon  = $this->Mainaddons->save($mainEntity);
+
+                   if (!empty($mainAddon)) {
+                       if(isset($postData['editid']) && !empty($postData['editid'])) {
+                           $this->Subaddons->deleteAll(
+                           [
+                               'Subaddons.mainaddons_id' => $postData['editid']
+                           ]);
+                       }
+
+                       if (!empty($this->request->getData('Main')['subaddons'])) {
+                           foreach ($this->request->getData('Main')['subaddons'] as $skey => $sval) {
+                               if (!empty($sval['subaddons_name']) && !empty($sval['subaddons_price'])) {
+                                   $subEntity = $this->Subaddons->newEntity();
+                                   $sub['mainaddons_id']   = $mainAddon->id;
+                                   $sub['category_id']     = $postData['category_id'];
+                                   $sub['subaddons_name']  = $sval['subaddons_name'];
+                                   $sub['subaddons_price'] = $sval['subaddons_price'];
+                                   $subPatch = $this->Subaddons->patchEntity($subEntity, $sub);
+                                   $this->Subaddons->save($subPatch);
+                                   unset($subPatch);
+                               }
+                           }
+                       }
+                       $this->Flash->success(__("Mainaddon details inserted successfully"));
+                       return $this->redirect(ADMIN_BASE_URL.'addons/index');
+                   }
+               } else {
+                   $this->set(compact('allAddons','categorylist','id'));
+                   $this->Flash->error(__("Mainaddon name already exist"));
+               }
+           } else {
+               $this->set(compact('allAddons','categorylist','id'));
+               $this->Flash->error(__("Please enter mainaddon name"));
+           }
+       }
+
+        $this->set(compact(['allAddons','categorylist','id']));
+   }
+ #------------------------------------------------------------------------------------------
+    /*Addon Status Change*/
+    public function ajaxaction() {
+
+        if($this->request->is('ajax')){
+            if($this->request->getData('action') == 'addonStatusChange'){
+                $addon         = $this->Mainaddons->newEntity();
+                $addon         = $this->Mainaddons->patchEntity($addon,$this->request->getData());
+                $addon->id     = $this->request->getData('id');
+                $addon->status = $this->request->getData('changestaus');
+                $this->Mainaddons->save($addon);
+
+                $this->set('id', $this->request->getData('id'));
+                $this->set('action', 'addonStatusChange');
+                $this->set('field', $this->request->getData('field'));
+                $this->set('status', (($this->request->getData('changestaus') == 0) ? 'deactive' : 'active'));
+            }
+        }
+    }
+ #------------------------------------------------------------------------------------------
+    /*Addon Delete*/
+    public function deleteAddon($id = null){
+
+        if($this->request->is('ajax')){
+            if($this->request->getData('action') == 'Addon'
+                && $this->request->getData('id') != ''){
+
+                $id = $this->request->getData('id');
+                $addon         = $this->Mainaddons->newEntity();
+                $addon         = $this->Mainaddons->patchEntity($addon,$this->request->getData());
+                $addon->id     = $this->request->getData('id');
+                $addon->delete_status = 'Y';
+                $this->Mainaddons->save($addon);
+
+                $dbConn = ConnectionManager::get('default');
+                $dbConn->update('subaddons', ['delete_status' => 'Y'], ['mainaddons_id' => $id]);
+
+                list($allAddons) = $this->index('Addon');
+                if($this->request->is('ajax')) {
+                    $action    = 'Addon';
+                    $this->set(compact('action', 'allAddons'));
+                    $this->render('ajaxaction');
+                }
+            }
+        }
+    }
+
+
+
 #---------------------------------------------------------------------------------------------
 }#class end...
